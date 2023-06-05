def08349a8d741b72cb3908fc8da82eb281e3973
Mumbi69 <winfriendmumbi69@gmail.com>
2023-06-05 20:49:42 +0000

    Python - Hello, World

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-
-# /**
-# * _print - prints python script
-# * Return: 0
-# */
-
-print('"Programming is like building a multilingual puzzle' "\n")
+print('"Programming is like building a multilingual puzzle')
