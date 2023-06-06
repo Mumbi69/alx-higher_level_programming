ffb0bd0176746034bdedef6581a3fc94b9a49828
Mumbi69 <winfriendmumbi69@gmail.com>
2023-06-06 22:39:57 +0000

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-
-    if b < 0:
-        a = 1 / a
-        b = -b
-
-    while b > 0:
-        if b & 1:
-            result *= a
-
-        a *= a
-        b >>= 1
-
-    return result
+    return a ** b
