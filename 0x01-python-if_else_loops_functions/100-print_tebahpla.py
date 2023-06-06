4b3008fca2bccb35a9b69d9c9e61372a3edf7932
Mumbi69 <winfriendmumbi69@gmail.com>
2023-06-06 22:50:34 +0000

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 output = ""
 for i in range(ord('z'), ord('a') - 1, -1):
-    output += "{}".format(chr(i) if i % 2 == 0 else chr(i).upper())
+    output += chr(i) if i % 2 == 0 else chr(i).upper()
 
-print(output)
+print(output, end="")
