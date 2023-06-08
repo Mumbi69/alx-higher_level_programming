ada05a5ac7be89b8aba11ef922002206e894c7b4
Mumbi69 <winfriendmumbi69@gmail.com>
2023-06-08 17:57:09 +0000

    Python - import & modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
+    argv = sys.argv[1:]
+
+    total = 0
+    for arg in argv:
+        total += int(arg)
+    print(total)
