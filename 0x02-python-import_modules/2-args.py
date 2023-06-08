b533ed0b61ca0664927e18409a5d4668a9daa8f5
Mumbi69 <winfriendmumbi69@gmail.com>
2023-06-08 17:09:33 +0000

    Python - import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,11 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
     argv = sys.argv[1:]
 
-    print("{} argument{}{}".format(argc, 's' if argc != 1 else '', ':' if argc > 0 else '.'))
+    print("{} argument{}{}".format(
+        argc,
+        's' if argc != 1 else '',
+        ':' if argc > 0 else '.'
+    ))
 
     for i, arg in enumerate(argv):
         print("{}: {}".format(i + 1, arg))
