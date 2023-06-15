5d54e988a2a6532fa438ffa15325e5eb95b70791
Mumbi69 <winfriendmumbi69@gmail.com>
2023-06-15 12:43:11 +0000

    Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,14 +11,14 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *node = h;
-    	size_t count = 0;
+	size_t count = 0;
 
-    	while (node != NULL)
-    	{
-        	printf("%d\n", node->n);
-        	count++;
-        	node = node->next;
-    	}
+	while (node != NULL)
+	{
+		printf("%d\n", node->n);
+		count++;
+		node = node->next;
+	}
 
-    	return (count);
+	return (count);
 }
