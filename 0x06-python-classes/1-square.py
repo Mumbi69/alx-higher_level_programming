96bcf02da49f667fd4192bac2ec8d29eecb2b0f5
Mumbi69 <winfriendmumbi69@gmail.com>
2023-06-27 20:49:09 +0000

    Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,9 +3,9 @@
 
 
 class Square:
-    """Represent a square."""
+    """Represents a square."""
 
     def __init__(self, size):
-        """Initialize the square qith the given size."""
+        """Initialize the square with the given size."""
 
         self.__size = size
