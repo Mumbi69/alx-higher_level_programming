692c90903c18a0ef2f1e5350e72635cdd4cffbf3
Mumbi69 <winfriendmumbi69@gmail.com>
2023-07-03 20:06:58 +0000

    More Classes and Objects

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -38,10 +38,12 @@ class Rectangle:
 
     def area(self):
         """Returns the rectangle area"""
-        return(self.__width * self.__height)
+        return (self.__width * self.__height)
 
     def perimeter(self):
         """ Calculates the perimeter of the rectangle."""
+        if self.__width == 0 or self.__height == 0 :
+            return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
