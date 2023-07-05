76d5b0ef10863bcf286770cd73651e4c5ad147b7
Mumbi69 <winfriendmumbi69@gmail.com>
2023-07-05 13:00:24 +0000

    Test-driven development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,7 +3,7 @@
 
 
 def text_indentation(text):
-    """This function prints a text with 2 new lines after each of these characters: ., ? and :"""
+    """Prints a text with 2 new lines after characters: ., ? and :"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
