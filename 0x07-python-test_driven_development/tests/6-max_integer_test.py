c1a4d9fa6fcfec929dcd890a0134decc9681af03
Mumbi69 <winfriendmumbi69@gmail.com>
2023-07-05 15:27:21 +0000

    Test-driven development

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Unittest fot max_integer."""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """My testcase that inherits from unittest.Testcase"""
+
+    def test_ordered_list(self):
+        """Test an ordered list of int."""
+        ordered = (1, 2, 3)
+        self.assertEqual(max_integer(ordered), 3)
+
+    def test_empty_list(self):
+        """Test if the list is empty"""
+        empty = ()
+        self.assertEqual(max_integer(empty), None)
+
+    def test_one_argument(self):
+        """Tests when only one argument is passed"""
+        One = [4]
+        self.assertEqual(max_integer(One), 4)
+
+    def test_unordered(self):
+        """Test if the list is unordered"""
+        unordered = (10, 20, 40, 30)
+        self.assertEqual(max_integer(unordered), 40)
+
+    def test_max_start(self):
+        """Tests a max integer at the beginning of list"""
+        max = (40, 30, 20, 10)
+        self.assertEqual(max_integer(max), 40)
+
+    def test_floats(self):
+        """Tests a list of floats for max"""
+        float = (1.0, 2.0, 3.0, 4.0, 5.5)
+        self.assertEqual(max_integer(float), 5.5)
+
+    def test_string(self):
+        """Tests when a string is passed"""
+        string = "Mumbi"
+
+
+
+
+
+
+
