4c762e0bc891071f7ee2da414c4bcd24146366a6
Mumbi69 <winfriendmumbi69@gmail.com>
2023-07-11 18:33:17 +0000

    Input/Output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a JSON representation."""
+
+
+import json
+"""Imports the JavaScript Object Notation data interchange format"""
+
+
+def to_json_string(my_obj):
+    """Represents the JSON data interchange format"""
+    return json.dumps(my_obj)
