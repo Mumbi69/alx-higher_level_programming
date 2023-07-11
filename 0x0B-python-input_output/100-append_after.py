09dcd6aa157c4baffc858197acaf15b964683c2a
Mumbi69 <winfriendmumbi69@gmail.com>
2023-07-11 20:57:14 +0000

    Input/Output

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Defines a line of text to a file containing a specific string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text to a file, after each line"""
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
