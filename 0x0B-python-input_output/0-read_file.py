66021c8e58520b8153d87682e49f0f28ccd462f0
Mumbi69 <winfriendmumbi69@gmail.com>
2023-07-11 15:34:41 +0000

    Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,10 +2,10 @@
     """Defines a text-file reading function."""
 
 
-    def read_file(filename=""):
-        """reads a text file (UTF8) and prints it to stdout:"""
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout:"""
 
-        with open(filename, "r", encoding="utf-8") as f:
-            """reads a text file (UTF8) and prints it to stdout."""
-            for line in f:
-                print(line, end='')
+    with open(filename, "r", encoding="utf-8") as f:
+        """reads a text file (UTF8) and prints it to stdout."""
+        for line in f:
+            print(line, end='')
