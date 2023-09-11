bdee15c8e7cdbb711c050b4264356870838414d8
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-11 21:44:38 +0000

    JavaScript - Warm up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('process');
+const argument = Number(process.argv[2]);
 function factorial (n) {
   if (isNaN(n)) {
     return 1;
@@ -9,5 +10,4 @@ function factorial (n) {
   }
   return n * factorial(n - 1);
 }
-const argument = Number(process.argv[2]);
 console.log(factorial(argument));
