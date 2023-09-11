36006b88763ba8d84e7269629ac8d5378a45780b
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-11 10:31:51 +0000

    JavaScript - Warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -6,12 +6,3 @@ if (process.argv.length === 2) {
 } else {
   console.log("Arguments found");
 }
-/*
-The process.argv variable is like a list (or an array)
-that holds all the arguments that were passed to your script
-when you started it.
-The first item in this list, process.argv[0]
-is always the path to the Node.js executable itself.
-The second item, process.argv[1], is the path to
-JavaScript file that is currently being run.
-*/
