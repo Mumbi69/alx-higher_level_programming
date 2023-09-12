b6702ae2b6baeaa69c7126d0d110a155854a8ab2
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-12 22:20:59 +0000

    Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const fs = require('fs');
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destinationFile = process.argv[4];
+
+fs.readFile(sourceFile1, 'utf8', (err, data1) => {
+  if (err) throw err;
+
+  fs.readFile(sourceFile2, 'utf8', (err, data2) => {
+    if (err) throw err;
+
+    const concatenatedData = data1 + data2;
+
+    fs.writeFile(destinationFile, concatenatedData, (err) => {
+      if (err) throw err;
+      console.log('Files concatenated successfully.');
+    });
+  });
+});
