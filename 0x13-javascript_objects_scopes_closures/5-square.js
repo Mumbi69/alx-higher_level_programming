c058c5b794b16ec1958e2600ec7dd47d1d7b1691
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-12 21:51:28 +0000

    Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -7,6 +7,31 @@ class Rectangle {
     }
   }
 
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+
+  rotate () {
+    if (this.width && this.height) {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
+    }
+  }
+
+  double () {
+    if (this.width && this.height) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+}
+
+module.exports = Rectangle;
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
