31135935d0ea98aab5fa09535fdc19a595d7201d
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-12 21:53:11 +0000

    Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+class Square {
+  constructor(size) {
+    this.width = size;
+    this.height = size;
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+class SquareWithCharPrint extends Square {
+  constructor(size) {
+    super(size);
+  }
+}
+
+module.exports = SquareWithCharPrint;
