3b1cd33f414510fca8eb4ae8ccda9fa4612888ea
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-13 10:45:33 +0000

    JavaScript - Warm up

--- 0x12-javascript-warm_up/100-let_me_const.js
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -1,13 +1,3 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
-  console.log(0);
-} else if (args.length === 1) {
-  console.log(0);
-} else {
-  const integers = args.map(Number).sort((a, b) => b - a);
-
-  console.log(integers[1]);
-}
+myVar = 333;
