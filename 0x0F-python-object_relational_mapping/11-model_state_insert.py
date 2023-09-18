258da4a52771f3b42d78930bab171d178188da18
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-18 21:56:26 +0000

    Object-relational mapping

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-prints the State object with the name passed as argument from
-the database hbtn_0e_6_usa
+adds the State object “Louisiana” to the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
