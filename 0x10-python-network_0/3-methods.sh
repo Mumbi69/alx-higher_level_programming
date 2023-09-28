ed371b81a99079999520c922fd1ce198254fc8c2
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-28 22:05:59 +0000

    Python - Network #0

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -sX OPTIONS -I $1 | grep -i "Allow" | awk '{print $2}'
+curl -sX OPTIONS -I $1 | grep -i 'allow' | awk -F ': ' '{print $2}'
