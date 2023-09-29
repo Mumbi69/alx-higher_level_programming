f3766b9234173085db6dcad4dbfb0da32a9a61d8
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-29 22:09:44 +0000

    Python - Network #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+ fetches https://alx-intranet.hbtn.io/status
+
+"""
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    print("Body response:")
+    print(f"\t- type:", type(response.text))
+    print(f"\t- content:", response.text)
