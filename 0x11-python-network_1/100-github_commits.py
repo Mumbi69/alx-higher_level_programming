1dac7bb9012c980c7acc1123e6d69555596be4df
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-29 23:29:32 +0000

    Python - Network #1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""
+takes 2 arguments in order to solve this challenge.
+
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./100-github_commits.py <repository> <owner>")
+        sys.exit(1)
+
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+
+    # Define the GitHub API endpoint for retrieving commits
+    api_url = f'https://api.github.com/repos/{owner}/{repository}/commits'
+
+    # Send a GET request to the GitHub API endpoint
+    response = requests.get(api_url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        commits = response.json()
+
+        # Print the 10 most recent commits
+        for commit in commits[:10]:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+
+    else:
+        print("Error:", response.status_code)
