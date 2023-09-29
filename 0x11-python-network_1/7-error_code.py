853d7a8d8e0f3a8ad36bb9437a35fdd4e86092dc
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-29 23:08:41 +0000

    Python - Network #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,9 +9,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
     response = requests.get(url)
-    response.raise_for_status()
 
     if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
