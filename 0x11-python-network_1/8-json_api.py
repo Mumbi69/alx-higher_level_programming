45253d385d0534442ab81881b19d9d00177f9de6
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-29 23:13:03 +0000

    Python - Network #1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {'q': q}
+
+    response = requests.post(url, data=data)
+    response.raise_for_status()
+
+    try:
+        user_data = response.json()
+
+        if user_data:
+            print(f"[{user_data['id']}] {user_data['name']}")
+        else:
+            print("No result")
+
+    except ValueError:
+        print("Not a valid JSON")
