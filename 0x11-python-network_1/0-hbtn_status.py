eee28894ea27a368e17a56c0bd706756d29bce84
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-29 20:06:50 +0000

    Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,8 @@
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
+            as response:
         html = response.read()
         utf8_html = html.decode('utf-8')
 
