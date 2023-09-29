738a3c90c3f1902df2ad82562975767bcfbe5b78
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-29 21:45:56 +0000

    Python - Network #1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+takes in a URL and an email, sends a POST request to
+the passed URL with the email as a parameter, and
+displays the body of the response (decoded in utf-8)
+
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    html = {'email': email}
+    html = urllib.parse.urlencode(html).encode('utf-8')
+    with urllib.request.urlopen(url, data=html) as response:
+        response.html = response.read().decode('utf-8')
+        print(response_data)
