f736caf7713cab85858f57c079a411072503fe90
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-29 20:04:05 +0000

    Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """first script to fetch URL"""
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
-    utf8_html = html.decode('utf-8')
 
-    print("Body response:")
-    print("    - type:", type(html))
-    print("    - content:", html)
-    print("    - utf8 content:", utf8_html)
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        utf8_html = html.decode('utf-8')
+
+        print("Body response:")
+        print("    - type:", type(html))
+        print("    - content:", html)
+        print("    - utf8 content:", utf8_html)
