72af5dfc32a089baca231db979b468046b80c476
Mumbi69 <winfriendmumbi69@gmail.com>
2023-09-29 21:58:46 +0000

    Python - Network #1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,8 +13,10 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    html = {'email': email}
-    html = urllib.parse.urlencode(html).encode('utf-8')
-    with urllib.request.urlopen(url, data=html) as response:
-        response.html = response.read().decode('utf-8')
+    data = {'email': email}
+    data = urllib.parse.urlencode(data).encode('utf-8')
+
+    req = urllib.request.Request(url, data=data, method='POST')
+    with urllib.request.urlopen(req) as response:
+        response_data = response.read().decode('utf-8')
         print(response_data)
