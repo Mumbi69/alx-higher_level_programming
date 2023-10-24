db99cba49a4162ee75ee815bdaddb434d33e746d
Mumbi69 <winfriendmumbi69@gmail.com>
2023-10-24 13:22:21 +0000

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-
 const filePath = process.argv[2];
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(data);
-});
+try {
+  const data = fs.readFileSync(filePath, 'utf8');
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
