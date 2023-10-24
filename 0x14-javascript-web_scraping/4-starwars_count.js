9030811555b2e19f768aedcc3b04da18f13c0bda
Mumbi69 <winfriendmumbi69@gmail.com>
2023-10-24 13:53:04 +0000

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@ const request = require('request');
 const apiUrl = process.argv[2];
 const characterId = 18;
 
-request(apiUrl, (error, response, body) => {
+request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
   } else if (response.statusCode === 200) {
