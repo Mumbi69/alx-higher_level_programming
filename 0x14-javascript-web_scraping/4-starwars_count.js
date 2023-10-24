15dd796f44ca8b473529384fd35fda02c10a3e8a
Mumbi69 <winfriendmumbi69@gmail.com>
2023-10-24 19:14:20 +0000

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
-const apiUrl = process.argv[2];
-const characterId = 18;
-
-request.get(apiUrl, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const filmsData = JSON.parse(body);
-    const count = filmsData.results.reduce((total, film) => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-        return total + 1;
-      } else {
-        return total;
-      }
-    }, 0);
-    console.log(count);
-  } else {
-    console.error(error);
-  }
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) console.log(err);
+  const count = body.split('/people/18/').length - 1;
+  console.log(count);
 });
