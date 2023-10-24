92aaba2de2d1344e496937c96c7e17bdefa00d7c
Mumbi69 <winfriendmumbi69@gmail.com>
2023-10-24 13:27:53 +0000

    JavaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+const content = process.argv[3];
+try {
+  fs.writeFileSync(filePath, content, 'utf8' );
+} catch (err) {
+  console.error(err);
+}
