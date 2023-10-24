4077d4737a955a7cbdea3fb2037341fbd0ae327a
Mumbi69 <winfriendmumbi69@gmail.com>
2023-10-24 13:35:40 +0000

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const request = require('request')
+const request = require('request');
 const url = process.argv[2];
 request(url, (error, response) => {
   if (error) {
-    console.log(error)
+    console.log(error);
   } else {
     console.log(`code: ${response.statusCode}`);
   }
